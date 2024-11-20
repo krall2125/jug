da847c5b6ee0734847c17837841315ef13d085aa
krall2125 <janrenski@gmail.com>
2024-11-20 18:52:53 +0100

    fixed possible segfault in file reading

--- src/main.c
+++ src/main.c
@@ -16,6 +16,11 @@ int main(int argc, char **argv) {
 	for (int i = 1; i < argc; i++) {
 		if (str_endswith(argv[i], ".jug")) {
 			char *contents = read_file(argv[i]);
+			
+			if (contents == NULL) {
+				continue;
+			}
+
 			printf("%s:\n%s\n", argv[i], contents);
 			free(contents);
 		}
