ddc1f008a2a2ac6393d8d8bb9e22b09403b17e10
krall2125 <janrenski@gmail.com>
2024-11-25 20:18:29 +0100

    added alloc and free macros bkdljsouiejnh

--- include/memory.h
+++ include/memory.h
@@ -9,4 +9,10 @@
 #define FREE_ARR(type, pointer, oldsz) \
 	reallocate(pointer, sizeof(type) * (oldsz), 0)
 
+#define ALLOC(type, size) \
+	(type *)reallocate(NULL, 0, sizeof(type) * size)
+
+#define FREE(type, oldsz, pointer) \
+	reallocate(pointer, oldsz, 0)
+
 void *reallocate(void *pointer, size_t old, size_t newsz);
